752d6cbcd23dfa1a26abdcbbb3d141043d2f4342
Ellen Ormerod <ellen.ormerod17@gmail.com>
2018-01-16 14:45:35 -0600

    final extension

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Chrome Extension
 
 This extension is for when I am are interested in emailing a friend the current link I am on.
-It provides a popup box with email, name and message inputs available and then sends the email for the user. 
+When the extension is clicked it opens a new tab in gmail with a compose mail form that has the subject line prefilled and the body of the message has the link in it. 
 
-Intent was to make my own basic chrome extension.
+Intent behind it was to make my own basic chrome extension.
+
+This was greatly helped by https://developer.chrome.com/extensions/samples "Email this page".
--- background.js
+++ background.js
@@ -1,41 +1,30 @@
-function mailUrl(){
-  if (window.localStorage == null) return "";
-  if (window.localStorage.mailUrl == null) return "";
-  console.log('mailUrl', window.localStorage);
-  return window.localStorage.mailUrl
-}
+var gmail = "https://mail.google.com/mail/?extsrc=mailto&url=%s";
 
+
+//3 this runs next because it was called in step 2. This puts together the message and then creates a new tab to open gmail in
 function sendMailto(tab_id, subject, body, selection){
-  let defaultHandler = mailUrl().length == 0;
-  console.log('what am i sending', tab_id, subject, body, selection)
+  window.localStorage.mailUrl = gmail;
+  let customUrl = window.localStorage.mailUrl
   let sendUrl = "mailto:?"
   if (subject.length > 0) sendUrl += "subject=" + encodeURIComponent(subject) + "&";
   if (body.length > 0) sendUrl += "body=" + encodeURIComponent(body);
   if (selection.length > 0) sendUrl += encodeURIComponent("\n\n") + encodeURIComponent(selection)
-  if (!defaultHandler){
-    //should open up gmail in separate tab
-    let customUrl = mailUrl();
-    sendUrl = customUrl.replace("%x", encodeURIComponent(sendUrl));
-    chrome.tabs.create({url: sendUrl})
-    console.log('no default handler')
-  } else {
-    //don't think i want because opens up in same tab
-    chrome.tabs.update(tab_id, {url: sendUrl});
-  }
-  console.log('final product', sendUrl)
+  //should open up gmail in separate tab
+  sendUrl = customUrl.replace("%s", encodeURIComponent(sendUrl));
+  chrome.tabs.create({url: sendUrl})
 }
 
+//2 then this runs because it has a listener on connect which comes from contentScript sending the message
 chrome.runtime.onConnect.addListener(function(port){
   let tab = port.sender.tab;
   port.onMessage.addListener(function(info){
-    console.log('info', info)
-    if (info.selection.length > 1024){
-      info.selection = info.selection.substring(0, 1024);
-      sendMailto(tab.id, info.title, tab.url, info.selection)
-    }
+    if (info.selection.length > 1024) info.selection = info.selection.substring(0, 1024);
+    sendMailto(tab.id, info.title, tab.url, info.selection);
   })
 })
 
+
+//1 when icon for chrome extension is clicked this runs first and goes into contentScript.js
 chrome.browserAction.onClicked.addListener(function(tab){
   if (tab.url.indexOf('http:') != 0 && tab.url.indexOf('https:') != 0){
     sendMailto(tab.id, "", tab.url, "");
@@ -43,31 +32,3 @@ chrome.browserAction.onClicked.addListener(function(tab){
     chrome.tabs.executeScript(null, {file: "contentScript.js"})
   }
 })
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
-let getCurrentTabUrl = (callback) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    let activeTab = tabs[0];
-    let url = activeTab.url;
-    callback(url)
-  })
-}
-
-let linkToSend
-document.addEventListener("DOMContentLoaded", () => {
-  getCurrentTabUrl((url) => {
-    linkToSend = url;
-  })
-})
-
-document.getElementById("submit_btn").addEventListener("click", clickFunction)
-
-function clickFunction(){
-  emailjs.send("gmail", "chrome_email_ext", {"email":document.getElementById("email_input").value,"to_name":document.getElementById("name_input").value,"message":document.getElementById("message_input").value,"link":linkToSend})
-  // window.open(`mailto:${document.getElementById("email_input").value}?subject=Link&body=${linkToSend}`)
-}
